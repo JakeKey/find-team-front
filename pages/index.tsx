b9af134500ec147c39692935c6a49c804b9fa0f9
Jakub Kozlowski <jkozlowski@speednet.pl>
2021-01-24 22:04:14 +0100

    Add typescript, eslint and prettier config

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  const test = 'asd';
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      index
+    </div>
+  );
+};
+
+export default Home;
